136d333087d78fc30cd505868b4a4ecbb3c9f0f5
devuros <urosjovanovic0704@gmail.com>
2018-02-02 22:45:36 +0100

    Finish footer component

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,1 +1,55 @@
-footer
+<div class='footer-content'>
+	<div class='footer-logo'>
+
+		<a href="{{ route('index') }}">
+			<i class='fa fa-fighter-jet'></i>
+		</a>
+
+	</div>
+	<div class='footer-text'>
+
+		<p>
+			&copy; 2018 Uros Jovanovic. All rights reserved.
+			All trademarks are property of their respective owners in Vadeem and other countries.
+			<br/>
+			VAT included in all prices where applicable.
+		</p>
+
+	</div>
+	<div class='footer-title'>
+
+		<a href="{{ route('index') }}">darcade</a>
+
+	</div>
+</div>
+<div class='footer-menu'>
+
+	<ul class="nav">
+		<li class="nav-item">
+
+			<a href="{{ route('index') }}">store</a>
+
+		</li>
+		<li class="nav-item">
+
+			<a href="{{ route('index') }}">community</a>
+
+		</li>
+		<li class="nav-item">
+
+			<a href="{{ route('about') }}">about darcade</a>
+
+		</li>
+		<li class="nav-item">
+
+			<a href="{{ route('support') }}">support</a>
+
+		</li>
+		<li class="nav-item">
+
+			<a href="{{ route('author') }}">author</a>
+
+		</li>
+	</ul>
+
+</div>
--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,8 @@ Route::get('about', function () { return view('index'); })->name('about');
 
 Route::get('support', function () { return view('index'); })->name('support');
 
+Route::get('author', function () { return view('index'); })->name('author');
+
 
 Route::get('login', function () { return view('index'); })->name('login');
 
