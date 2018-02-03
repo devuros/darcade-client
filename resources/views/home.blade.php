1d8e637dc8eb32708fb505cc21db3b0bea8cf2e6
devuros <urosjovanovic0704@gmail.com>
2018-02-03 17:02:10 +0100

    Finish gutter on home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,11 +6,67 @@
 
 	<div class='container home-page-content-wrapper'>
 		<div class='home-page-gutter'>
+			<div class='gutter-icon'>
 
-			<strong>gutter</strong>
+				<i class='fa fa-crosshairs fa-4x'></i>
 
+			</div>
+			<div class="gutter-wrapper">
+				<div class='gutter-header'>browse categories</div>
+				<div class='gutter-items'>
+
+					<a class="gutter-item" href="#">
+						<div>
+							<i class='fa fa-line-chart'></i>
+						</div>
+						<b class="item-text">top sellers</b>
+					</a>
+					<a class="gutter-item" href="#">
+						<div>
+							<i class='fa fa-plus'></i>
+						</div>
+						<b class="item-text">new releases</b>
+					</a>
+					<a class="gutter-item" href="#">
+						<div>
+							<i class='fa fa-percent'></i>
+						</div>
+						<b class="item-text">specials</b>
+					</a>
+
+				</div>
+				<div class='gutter-header gutter-genres'>browse by genre</div>
+				<div class='gutter-items'>
+
+					<a class="gutter-item" href="#">
+						<b class="item-text">action</b>
+					</a>
+					<a class="gutter-item" href="#">
+						<b class="item-text">adventure</b>
+					</a>
+					<a class="gutter-item" href="#">
+						<b class="item-text">indie</b>
+					</a>
+					<a class="gutter-item" href="#">
+						<b class="item-text">simulation</b>
+					</a>
+					<a class="gutter-item" href="#">
+						<b class="item-text">massively multiplayer</b>
+					</a>
+					<a class="gutter-item" href="#">
+						<b class="item-text">strategy</b>
+					</a>
+					<a class="gutter-item" href="#">
+						<b class="item-text">RPG</b>
+					</a>
+					<a class="gutter-item" href="#">
+						<b class="item-text">sports</b>
+					</a>
+
+				</div>
+			</div>
 		</div>
-		<div class='home-page-core'>
+		<div class='home-page-core-wrapper'>
 
 			<p>Content</p>
 
