794b48de400d96fc4ea5d3c857efd8cba12ed23b
devuros <urosjovanovic0704@gmail.com>
2018-02-17 21:08:51 +0100

    Prepare SearchController and views

---
+++ app/Http/Controllers/SearchController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SearchController extends ClientController
+{
+
+    public function __construct()
+    {
+    	//
+    }
+
+    public function index()
+    {
+    	//
+    }
+
+    public function topSellers()
+    {
+    	//
+    }
+
+    public function newReleases()
+    {
+    	//
+    }
+
+    public function specials()
+    {
+    	//
+    }
+
+    public function underTen()
+    {
+    	//
+    }
+
+    public function underTwentyFive()
+    {
+    	//
+    }
+
+}
